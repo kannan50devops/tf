da6dce34cda27de71f3bb89855024e1803a07c9f
kannan50devops <122429314+kannan50devops@users.noreply.github.com>
2023-03-27 19:15:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "euran-terraform-statefile2"
+    bucket = "euran-terraform-statefile3"
     key = "eks/ngg_cluster_name/statefile"
     region = "ap-south-1"
   }
